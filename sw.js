2c26a279794706f122a4e820f5ebfec0691400d0
marinhrustek-gif <marinhrustek@gmail.com>
2025-10-15 14:05:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "nt-online-v1";
+const ASSETS = ["./","./index.html","./config.html","./app.js","./config.json","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",(e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener("activate",(e)=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
+self.addEventListener("fetch",(e)=>{ e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request))); });
